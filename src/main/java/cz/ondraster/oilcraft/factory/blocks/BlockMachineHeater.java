4adfb23d6acb8676957068669284549a3e923b76
Ondra Moravek <ondra.moravek@hotmail.com>
2014-07-07 15:33:17 +0200

    Some work on multiblocks

---
+++ src/main/java/cz/ondraster/oilcraft/factory/blocks/BlockMachineHeater.java
@@ -0,0 +1,9 @@
+package cz.ondraster.oilcraft.factory.blocks;
+
+import cz.ondraster.oilcraft.References;
+
+public class BlockMachineHeater extends MultiblockPart {
+   public BlockMachineHeater() {
+      setBlockName(References.UnlocalizedNames.BLOCKELECTRICHEATER);
+   }
+}
--- src/main/java/cz/ondraster/oilcraft/factory/blocks/TileEntityTreater.java
+++ src/main/java/cz/ondraster/oilcraft/factory/blocks/TileEntityTreater.java
@@ -1,5 +1,6 @@
 package cz.ondraster.oilcraft.factory.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityTreater extends TileEntityController {
@@ -21,12 +22,43 @@ public class TileEntityTreater extends TileEntityController {
 
       // bottom row = either all of them are solid heaters or all of them are electric heaters. Should be 1 below us.
 
-      ForgeDirection orientationController = ForgeDirection.getOrientation(worldObj.getBlockMetadata(xCoord, yCoord, zCoord)).getOpposite();
+      ForgeDirection orientationController = ForgeDirection.getOrientation(worldObj.getBlockMetadata(xCoord, yCoord, zCoord));
+      ForgeDirection opposite = orientationController.getOpposite();
 
-      for (int x = xCoord - orientationController.getOpposite().offsetX; x <= xCoord + orientationController.offsetX; x++) // check for the other orientation later
-      {
-         for (int z = zCoord - orientationController.offsetZ; z <= zCoord; z++) {
-            System.out.println("Checking block at: x = " + x + ", y = " + yCoord + ", z = " + z);
+      boolean isOk = true;
+      Block bottomHeater = null;
+
+      if (orientationController == ForgeDirection.EAST || orientationController == ForgeDirection.WEST) {
+         for (int z = zCoord - 1; z <= zCoord + 1; z++) {
+            int a = xCoord + opposite.offsetX;
+            int b = xCoord;
+            for (int x = Math.min(a, b); x <= Math.max(a, b); x++) {
+               System.out.println("Checking block at: x = " + x + ", y = " + yCoord + ", z = " + z);
+               Block block = worldObj.getBlock(x, yCoord - 1, z);
+               if (bottomHeater != null && block != bottomHeater) {
+                  isOk = false;
+               } else if (!(block instanceof BlockMachineHeater)) {
+                  isOk = false;
+               } else {
+                  bottomHeater = block;
+               }
+            }
+         }
+      } else {
+         for (int x = xCoord - 1; x <= xCoord + 1; x++) {
+            int a = zCoord + opposite.offsetZ;
+            int b = zCoord;
+            for (int z = Math.min(a, b); z <= Math.max(a, b); z++) {
+               System.out.println("Checking block at: x = " + x + ", y = " + yCoord + ", z = " + z);
+               Block block = worldObj.getBlock(x, yCoord - 1, z);
+               if (bottomHeater != null && block != bottomHeater) {
+                  isOk = false;
+               } else if (!(block instanceof BlockMachineHeater)) {
+                  isOk = false;
+               } else {
+                  bottomHeater = block;
+               }
+            }
          }
       }
 
