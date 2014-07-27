c6f0077c9eae73515927edaf4ad08182a2e9b50a
Ondra Moravek <ondra.moravek@hotmail.com>
2014-07-27 18:59:54 +0200

    Added IToolWrench to OilWrench and IC2 API

--- src/main/java/cz/ondraster/oilcraft/items/OilWrench.java
+++ src/main/java/cz/ondraster/oilcraft/items/OilWrench.java
@@ -1,13 +1,27 @@
 package cz.ondraster.oilcraft.items;
 
+import buildcraft.api.tools.IToolWrench;
+import cpw.mods.fml.common.Optional;
 import cz.ondraster.oilcraft.OilCraft;
 import cz.ondraster.oilcraft.References;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 
-public class OilWrench extends Item {
+@Optional.Interface(iface = "IToolWrench", modid = "BuildCraftAPI|tools")
+public class OilWrench extends Item implements IToolWrench {
    public OilWrench() {
       setUnlocalizedName(References.UnlocalizedNames.ITEMWRENCH);
       setTextureName(References.Icons.ITEMWRENCH);
       setCreativeTab(OilCraft.creativeTab);
+   }
+
+   @Override
+   public boolean canWrench(EntityPlayer player, int x, int y, int z) {
+      return true;
+   }
+
+   @Override
+   public void wrenchUsed(EntityPlayer player, int x, int y, int z) {
+
    }
 }
